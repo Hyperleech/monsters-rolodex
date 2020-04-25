8aab0b31d3c69538e7cf09dd4c26d238a62bbcc3
Robert Cherenkov <comandante@Roberts-MacBook-Pro.local>
2020-04-25 18:50:14 +0300

    Updates

---
+++ precache-manifest.c1f2b53a12364898387d207827b2f8c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3341f58e94d0b76f4c6b952dcd2b02a4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "370d9cfa5e9156af3cf5",
+    "url": "/monsters-rolodex/static/css/main.1289802d.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monsters-rolodex/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "370d9cfa5e9156af3cf5",
+    "url": "/monsters-rolodex/static/js/main.d32b91fa.chunk.js"
+  },
+  {
+    "revision": "96e4dc8f64b3356b837b",
+    "url": "/monsters-rolodex/static/js/runtime-main.6b89a6eb.js"
+  }
+]);
